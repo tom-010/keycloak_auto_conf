85c059d71cdff3dd591d9c2e6dd00aa9ed22b85e
Thomas Deniffel <tdeniffel@gmail.com>
2022-06-16 13:18:32 +0200

    create admin on container build: problem admin-password is artifact

--- scripts/add_admin.sh
+++ scripts/add_admin.sh
@@ -4,5 +4,6 @@ set -e
 
 export $(cat .env | xargs)
 dockerComposeName=${PWD##*/}
-docker exec ${dockerComposeName}_auth_1 /opt/jboss/keycloak/bin/add-user-keycloak.sh -u $KEYCLOAK_USER -p $KEYCLOAK_PASSWORD
-docker-compose restart auth
+#docker exec ${dockerComposeName}_auth_1 /opt/jboss/keycloak/bin/add-user-keycloak.sh -u $KEYCLOAK_USER -p $KEYCLOAK_PASSWORD
+docker exec ${dockerComposeName}_auth_1 cat /tmp/out
+#docker-compose restart auth
